9a99acfc2ee04473f81871ea4d3ff12a4d0535d9
Mustafa Ahmed <musi1996@yahoo.com>
2024-04-11 19:16:44 -0700

    Finshed adding Modal to HTML/CSS/JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
+// Mustafa- Functionality for modal Button below
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
 const getRandomMoviesButton = document.getElementById('RandomMovies'); //get the button to desiplay random movies
 const movieList = document.getElementById('movieList'); //get the list element with the id "movieList"
 
@@ -39,3 +70,4 @@ function getRandomElements(array, n) {
     const shuffled = array.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, n);
 }
+
