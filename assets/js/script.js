2bec130a28a95862d7b5a99b5d17e3a3f1097b17
LBenjamin <Lrbenjamin22@gmail.com>
2024-04-14 18:28:19 -0700

    Updated the api function again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 const getRandomMoviesButton = document.getElementById('getRandomMoviesButton');
+const movieAPI = "https://api.themoviedb.org/3/discover/movie?language=en&with_genres=28,16,27"
+const oldAPI = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&language=en-US&page=1'
+
 
 getRandomMoviesButton.addEventListener('click', function() {
     const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzgzM2Y1YWYwZGYzN2NiMzJlZWMzODNmZDY3MDBlYiIsInN1YiI6IjY2MTVmM2U0Mzk3ZGYwMDE3ZGM4YzA0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-oNPj8d96s08rtB6JLbcFxCXWF7pT_V2hkcxrMtHgqg'; 
     
-    fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&language=en-US&page=1', {
+    fetch(movieAPI, {
         headers: {
             'Authorization': `Bearer ${accessToken}`
         }
@@ -16,7 +19,7 @@ getRandomMoviesButton.addEventListener('click', function() {
         const movieContainer = document.getElementById('moviesContainer');
         movieContainer.innerHTML = ''; 
         const moviesList = document.createElement('ul');
-        data.results.forEach(function(movie) {
+        data.results.forEach(function(movie) { //for loop to make 5 movies appear
             const listItem = document.createElement('li');
             listItem.textContent = movie.title;
             moviesList.appendChild(listItem);
@@ -32,5 +35,10 @@ getRandomMoviesButton.addEventListener('click', function() {
             moviesList.appendChild(listItem)
         });
         movieContainer.appendChild(moviesList);
+        for (let index = 0; index < 5; index++) {
+        console.log(data.results[index])    
+        }
     });
 });
+ //
+ //
