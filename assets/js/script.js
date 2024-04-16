81e5549e0a9cb5a49b3294fd00b8692c592045e8
Caleb Warford <cjwarford98@gmailcom>
2024-04-15 20:14:53 -0700

    working on modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-// Mustafa- Functionality for modal Button below
-// Get the modal
-const modal = document.getElementById("myModal");
+// // Mustafa- Functionality for modal Button below
+// // Get the modal
+// const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 
 
@@ -73,3 +73,32 @@ getRandomMoviesButton.addEventListener('click', function() {
         }
   });
 });
+
+// Function to show the modal
+function showModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
+
+// Event listener for the close button
+document.getElementById('closeModal').addEventListener('click', closeModal);
+
+// Logic to check if 3 genres are selected and show the modal
+function handleSave() {
+  const selectedGenres = []; // Add your logic to get selected genres
+
+  if (selectedGenres.length !== 3) {
+    showModal();
+  } else {
+    // Proceed with saving the genres
+  }
+}
+
+// Add event listener to the save button
+document.getElementById('getRandomMoviesButton').addEventListener('click', handleSave);
