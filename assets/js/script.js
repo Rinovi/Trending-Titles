130fd0958424c5cffdb8c315de78240f6a922224
LBenjamin <Lrbenjamin22@gmail.com>
2024-04-15 22:59:21 -0700

    Chuck Norris has entered the chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,47 @@
-// Mustafa- Functionality for modal Button below
-// Get the modal
-const modal = document.getElementById("myModal");
+// // Mustafa- Functionality for modal Button below
+// // Get the modal
+// const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 
-//
+$(document).ready(function() {
+  $.ajax({
+      method: 'GET',
+      url: 'https://api.api-ninjas.com/v1/chucknorris',
+      headers: { 'X-Api-Key': 'EuAua3ZW1gqABMQbdhxj1A==VKnZrE0wy6ft3Vgf'},
+      contentType: 'application/json',
+      success: function(result) {
+          console.log('Chuck Norris joke API response:', result);
+          $('#ChuckNorrisJoke').text(result.value); 
+      },
+      error: function ajaxError(jqXHR) {
+          console.error('Error: ', jqXHR.responseText);
+      }
+  });
+});
+
 
 const getRandomMoviesButton = document.getElementById('getRandomMoviesButton');
 const movieAPI = `https://api.themoviedb.org/3/discover/movie?language=en&with_genres=27`
@@ -76,3 +91,6 @@ getRandomMoviesButton.addEventListener('click', function() {
         }
   });
 });
+
+
+
