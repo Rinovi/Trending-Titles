9a80c3d2b06c962a83c3ddfb580bf652ba2ca5e5
Caleb Warford <cjwarford98@gmail.com>
2024-04-17 16:35:57 -0700

    Fixed 2 genres that weren't working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let genresId;
-let genresList=['action','crime','drama','fantasy', 'horror', 'comedy','romance','science-fiction','sports','thriller','mystery', 'war','western', 'anime', 'foreign', 'musical']
+let genresList=['action','crime','drama','fantasy', 'horror', 'comedy','romance','science-fiction','documentary','thriller','mystery', 'war','western', 'anime', 'tv-movie', 'musical']
 
 $(document).ready(function() {
   $.ajax({
